07cf6566df4a2fd0023ee750cf0dedd824f78c33
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-09-24 20:43:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import Home from '../components/Home';
-import Top from '../components/Top';
-import Contact from '../components/Contact';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
-
-import App from "./App";
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <App/>,
-    children : [
-      {
-        path: "/",
-        element: <Home/>
-      },
-      {
-        path:"/top-10",
-        element:<Top/>
-      },
-      {
-        path : '/contact',
-        element:<Contact/>
-      }
-    ]
-  }
-])
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById("root")
 );
