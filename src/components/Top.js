a968a4790bb390fadf279c5cca2f99d28d1ed204
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-09-24 20:41:01 +0530

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,32 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+
 function Top() {
     const [cryptoArr, setCryptoArr] = useState([]);
-    useEffect(()=>{
-      axios.get('https://api.coinlore.net/api/tickers/').then((response)=>{
-        setCryptoArr(response.data.data)
-      })
-    },[]) 
 
-    return (
-        <div className='top-container'>
-            <h1>Top 10 Global Crypto Coins</h1>
-            <div className='display-container'>
+  const url = "https://api.coinlore.net/api/tickers/";
 
-               {cryptoArr && cryptoArr.filter((item)=>{
-                  return item.rank <= 10
-               }).map((item,index)=>(
-                  <div key={index} className='coin-container'>
-                    <p className='rank-para'>Rank # {item.rank}</p>
-                    <h3 className='coin-symbol'>{item.name} ({item.symbol})</h3>
-                    <p className='price-para'>Price: {item.price_usd}</p>
-                  </div>
-               ))
-                }
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCryptoArr(data.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-            </div>
-        </div>
-    )
+  return (
+    <div className="top-container">
+      <h1>Top 10 Global Crypto Coins</h1>
+      <div className="display-container">
+        {cryptoArr &&
+          cryptoArr
+            .filter((item) => {
+              return item.rank <= 10;
+            })
+            .map((item, idx) => (
+              <div key={idx} className="coin-container">
+                <p className="rank-para">Rank # {item.rank}</p>
+                <h3 className="coin-symbol">
+                  {item.name} ({item.symbol})
+                </h3>
+                <p className="price-para">Price: {item.price_usd}</p>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
 }
 
 export default Top
