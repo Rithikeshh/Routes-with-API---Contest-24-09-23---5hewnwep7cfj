d392e87e83589f082b42b63551c454f52b4608d0
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-09-24 20:40:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-import "./styles.css";
-// import Data from './data'
-
 import React from 'react'
+import '../styles/App.css';
 import Navbar from './Navbar';
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './Home';
 import Top from './Top';
 import Contact from './Contact';
@@ -13,7 +11,14 @@ const App = () => {
   return (
     <div id="main">
       <Navbar />
-      <div className='container'><Outlet/></div>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/top-10" element={<Top />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   )
 }
